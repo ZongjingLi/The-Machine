3cc0fbe0f0bea0102a3cf9fbbeb25eafae039fe4
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 09:19:28 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,8 +4,11 @@ import torch.nn as nn
 from torch.utils.data import Dataset, dataloader
 
 class SpriteData(Dataset):
-    def __init__(self):
+    def __init__(self,split = "train"):
         super().__init__()
 
-    def forward(self,x):
-        return x
+        assert split in ["train","test","val"],print("Unknow dataset split type: {}".format(split))
+    def __len__(self):return 0
+
+    def __getitem__(self,index):
+        return 
