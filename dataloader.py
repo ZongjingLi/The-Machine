7f10a28fd32330cb089b4e7837e4f8b1e4179ca9
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 09:20:50 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -6,9 +6,13 @@ from torch.utils.data import Dataset, dataloader
 class SpriteData(Dataset):
     def __init__(self,split = "train"):
         super().__init__()
-
-        assert split in ["train","test","val"],print("Unknow dataset split type: {}".format(split))
+        assert split in ["train","test","val"],print("Unknown dataset split type: {}".format(split))
     def __len__(self):return 0
 
     def __getitem__(self,index):
         return 
+
+class BattlecodeData(Dataset):
+    def __init__(self,split = "train"):
+        super().__init__()
+        assert split in [],print("Unknown dataset split type: {}".format(split))
