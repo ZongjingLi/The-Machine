76e3759cc897ba8c370646b3d6d140f6d7fce46b
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-19 01:19:23 +0800

    Update ratplan.py

--- ratplan.py
+++ ratplan.py
@@ -6,10 +6,10 @@ from melkor_logic.NLM.model import *
 
 from config import *
 
-struct_config = (
-    (3,12,3 ),
-    (30,50,20),
-    (20,30,3 ))
+struct_config = ((3 ,12,3 ),
+                 (30,50,20),
+                 (20,30,3 ),
+                 (1 ,30,3 ))
 
 class RatSkill(nn.Module):
     def __init__(self,name = "default_name"):
@@ -29,8 +29,6 @@ class FiniteStateConstructor(nn.Module):
         super().__init__()
     def construct(self,inputs):return inputs
 
-skill = RatSkill()
-
 class RRT(nn.Module):
     def __init__(self):
         super().__init__()
@@ -42,3 +40,5 @@ class AStartSearch(nn.Module):
         super().__init__()
 
     def forward(self,x):return x
+
+skill = RatSkill()
