f271f215802f352aba07b767852c45590491d688
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-18 10:19:07 +0800

    Violet Evergarden

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--name","Violet Evergarden",type = str, help = "the Violet Evergarden")
+parser.add_argument("--name",default = "Violet Evergarden",type = str, help = "the Violet Evergarden")
 
 config = parser.parse_args(args = [])
--- ratplan.py
+++ ratplan.py
@@ -1,8 +1,16 @@
 import torch
 import torch.nn as nn
-
 import matplotlib.pyplot as plt
 
+from melkor_logic.NLM.model import *
+
+from config import *
+
 class RatSkill(nn.Module):
-    def __init__(self,config):
+    def __init__(self,name = "default_name"):
         super().__init__()
+        self.name = name
+        self.start_condition = NeuroLogicMachine()
+        self.end_condition = NeuroLogicMachine()
+
+skill = RatSkill()
