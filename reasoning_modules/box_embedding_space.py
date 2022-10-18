405d5342d865e9ef1fe3677341522474d94f1569
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-19 00:52:13 +0800

    Update box_embedding_space.py

--- reasoning_modules/box_embedding_space.py
+++ reasoning_modules/box_embedding_space.py
@@ -7,8 +7,12 @@ blue = ConceptBox("blue","color")
 red  = ConceptBox("red" ,"color")
 
 e1   = EntityBox(torch.randn([1,100]))
-
+e2   = EntityBox(torch.randn([1,100]))
 
 lp = logJointVolume(e1,blue,True)
 
 print(lp)
+
+print(calculate_categorical_log_pdf(e1,[blue,red]))
+
+print(calculate_filter_log_pdf([e1,e2],red))
