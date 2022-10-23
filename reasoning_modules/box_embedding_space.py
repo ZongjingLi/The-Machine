cdb41b26807a7d9c385d7b3b3913e88a9bc80aac
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-24 02:36:40 +0800

    Update box_embedding_space.py

--- reasoning_modules/box_embedding_space.py
+++ reasoning_modules/box_embedding_space.py
@@ -12,17 +12,28 @@ e2   = EntityBox(torch.randn([1,100]))
 lp = logJointVolume(e1,blue,True)
 
 class QuasiExecutor(nn.Module):
-    def __init__(self):
+    def __init__(self,concepts):
         super().__init__()
+        self.static_concepts = concepts["static_concepts"]
+        self.dynamic_concepts = concepts["dynamic_concepts"]
+        self.relations = concepts["relations"]
 
     def forward(self,program,context):
         if isinstance(program,str):program = toFuncNode(program)
+        """
+        the general scheme of the context is that there is always such terms in the
+        result diction that have key:
+            objects: the object features of perception
+            scores:  the probability of each feature represents an actual object
+        """
         def execute_node(node):
             if node.token == "scene":return context
             if node.token == "filter":return context
             return 0
         results = execute_node(program,context)
         return results
+
+
 
 print(lp)
 
