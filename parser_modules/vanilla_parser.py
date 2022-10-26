9d7b133b87206908084bc72ed84e4c418f1318d7
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-27 07:56:39 +0800

    Update vanilla_parser.py

--- parser_modules/vanilla_parser.py
+++ parser_modules/vanilla_parser.py
@@ -4,15 +4,28 @@ import torch.nn as nn
 from melkor_parser import *
 
 rule_diction = {
-    "filter":{"input_types":["object","concept"]},
+    "filter":{"input_types":["object_set","concept"]},
     "scene":{"input_types":[]},
     "red":{"input_types":None},
+    "exist":{"input_types":["object_set"]},
 }
 
-machine_cfg     = ContextFreeGrammar(rule_diction,64)
-program_decoder = Decoder(128,64,132,machine_cfg)
+def dfs_seq(program):
+    if isinstance(program,str):program = toFuncNode(program)
+    outputs = []
+    def dfs(node):
+        outputs.append(node.token)
+        for c in node.children:dfs(c)
+    dfs(program)
+    while "" in outputs:outputs.remove("")
+    return outputs
 
-parser = LanguageParser(64,128,program_decoder,corpus = ["what is that"])
+def make_program_parser(corpus,cfg_diction,word_dim=256,signal_dim=32,key_dim=42,latent_dim=132):
+    cfg = ContextFreeGrammar(cfg_diction,signal_dim)
+    decoder = Decoder(signal_dim,key_dim,latent_dim,cfg)
+    model = LanguageParser(word_dim,signal_dim,decoder,corpus)
+    return model
 
-outputs,lp = parser("what is that")
+parser =  make_program_parser(["what is that","is there any red object"],rule_diction)
+outputs,lp = parser("is there any red object",dfs_seq("exist(filter(scene(),red))"))
 print(outputs,lp)
