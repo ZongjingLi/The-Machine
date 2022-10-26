2b22e773c0e2f43cb9d69cc6ef4aca6b3977c691
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-27 07:42:19 +0800

    Update vanilla_parser.py

--- parser_modules/vanilla_parser.py
+++ parser_modules/vanilla_parser.py
@@ -3,4 +3,16 @@ import torch.nn as nn
 
 from melkor_parser import *
 
-LanguageParser(64,128,None,corpus = [])
+rule_diction = {
+    "filter":{"input_types":["object","concept"]},
+    "scene":{"input_types":[]},
+    "red":{"input_types":None},
+}
+
+machine_cfg     = ContextFreeGrammar(rule_diction,64)
+program_decoder = Decoder(128,64,132,machine_cfg)
+
+parser = LanguageParser(64,128,program_decoder,corpus = ["what is that"])
+
+outputs,lp = parser("what is that")
+print(outputs,lp)
