af9fa59dd3a6b389c2a18ee43c141df4264dfc0c
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-27 08:12:51 +0800

    update

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--name",default = "Violet Evergarden",type = str, help = "the Violet Evergarden")
-
+parser.add_argument("--name", default   = "Violet Evergarden",type = str, help = "the Violet Evergarden")
+parser.add_argument("--corpus",default  = "data/corpus.txt"  ,type = str)
+parser.add_argument("--grammar",default = "data/grammar.json",type = str)
 config = parser.parse_args(args = [])
--- parser_modules/vanilla_parser.py
+++ parser_modules/vanilla_parser.py
@@ -1,14 +1,7 @@
 import torch
 import torch.nn as nn
-
+from moic.utils    import save_json
 from melkor_parser import *
-
-rule_diction = {
-    "filter":{"input_types":["object_set","concept"]},
-    "scene":{"input_types":[]},
-    "red":{"input_types":None},
-    "exist":{"input_types":["object_set"]},
-}
 
 def dfs_seq(program):
     if isinstance(program,str):program = toFuncNode(program)
@@ -26,6 +19,14 @@ def make_program_parser(corpus,cfg_diction,word_dim=256,signal_dim=32,key_dim=42
     model = LanguageParser(word_dim,signal_dim,decoder,corpus)
     return model
 
-parser =  make_program_parser(["what is that","is there any red object"],rule_diction)
-outputs,lp = parser("is there any red object",dfs_seq("exist(filter(scene(),red))"))
-print(outputs,lp)
+if __name__ == "__main__":
+    rule_diction = {
+    "filter":{"input_types":["object_set","concept"]},
+    "scene":{"input_types":[]},
+    "red":{"input_types":None},
+    "exist":{"input_types":["object_set"]},
+    }
+    save_json(rule_diction,"grammar.json")
+    parser =  make_program_parser(["what is that","is there any red object"],rule_diction)
+    outputs,lp = parser("is there any red object",dfs_seq("exist(filter(scene(),red))"))
+    print(outputs,lp)
