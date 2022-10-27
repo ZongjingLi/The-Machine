42de807c2fc9512a2474b3e0223575ebd5b3e678
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-27 11:33:44 +0800

    ebm concept learner

--- machine_prototypes/ebm_learner.py
+++ machine_prototypes/ebm_learner.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
-from   perception_modules.energy_base import *
+from  perception_modules.energy_base import *
+from  parser_modules.vanilla_parser  import *
+from  reasoning_modules.box_embedding_space import *
 
 import argparse
 
@@ -12,12 +14,25 @@ class EBMLearner(nn.Module):
         super().__init__()
 
         # 1. the energy based perception module, input the image and output the 
-
+        self.component_model = LatentEBM(ebm_config)
+        self.ebm_models = [self.component_model for _ in range(ebm_config.components)]
 
         # 2. the symbolic program parser from language to program
-        
+        rule_diction = load_json(config.grammar)
+        corpus       = []
+        with open(config.corpus,"r") as corpus_file:
+            for line in corpus_file:corpus.append(line.strip())
+        # make the vanilla program parser made by ZongjingLi
+        self.program_parser = make_program_parser(corpus,rule_diction)
+
         # 3. quasi-symbolic concept program executor
 
+    def perception(self,im):
+        return im
+
+    def ground_concept(self,image,program,answer):
+        if isinstance(program,str): program = toFuncNode(program)
+        return image
 
     def forward(self,x):
         return x
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import torch.nn as nn
 if __name__ == "__main__":
     from config import *
     from machine_prototypes.object_centric_learner import *
+    from machine_prototypes.ebm_learner import *
     LORL = Lorl(opt = config)
+    EBML = EBMLearner(config)
