8c13b1978c52ad58d355b983626e71cb56ff0aec
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-27 11:23:49 +0800

    Update ebm_learner.py

--- machine_prototypes/ebm_learner.py
+++ machine_prototypes/ebm_learner.py
@@ -1,10 +1,23 @@
 import torch
 import torch.nn as nn
+from   perception_modules.energy_base import *
 
+import argparse
+
+model_parser = argparse.ArgumentParser()
+model_config = model_parser.parse_args(args = [])
 
 class EBMLearner(nn.Module):
     def __init__(self,config):
         super().__init__()
 
+        # 1. the energy based perception module, input the image and output the 
+
+
+        # 2. the symbolic program parser from language to program
+        
+        # 3. quasi-symbolic concept program executor
+
+
     def forward(self,x):
-        return z
+        return x
