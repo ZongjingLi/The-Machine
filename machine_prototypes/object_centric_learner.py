748d6a5651128925d02ce4931d773ecf8078f8c7
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-27 08:03:41 +0800

    update

--- machine_prototypes/object_centric_learner.py
+++ machine_prototypes/object_centric_learner.py
@@ -1,15 +1,18 @@
 import torch
 import torch.nn as nn
-
 import matplotlib.pyplot as plt
-# namomo
 from moic.data_structure import *
+
+from parser_modules.vanilla_parser import make_program_parser
+from reasoning_modules.box_embedding_space import *
+from perception_modules.savis import *
+
 # this is the machine
 class Lorl(nn.Module):
     def __init__(self,opt):
         super().__init__()
         self.visual_scene_parser = None
-        self.program_parser = None
+        self.program_parser = make_program_parser()
         self.knowledgebase = None
     
     def perception(self,image):
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 
 if __name__ == "__main__":
-    from dataloader import *
-    from perception_modules.savis import *
-    parser = SlotAttentionParser(6,100,3)
-    dataset = SpriteData("train")
-    train_loader = DataLoader(dataset,batch_size = 2)
+    from config import *
+    from machine_prototypes.object_centric_learner import *
+    LORL = Lorl(opt = config)
