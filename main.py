764a0f9968081f728b6fc31edeee29dd475f44ea
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-29 07:46:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import torch
 import torch.nn as nn
 
+def ground_results(results,answers):
+    outputs = []
+    for i in range(len(results)):
+        assert answers[i] in results[i]["outputs"]
+        index_answer = results[i]["outputs"].index(answers[i])
+        outputs.append(results[i]["scores"][index_answer])
+    return outputs
+
 if __name__ == "__main__":
     from config import *
     from machine_prototypes.object_centric_learner import *
@@ -8,5 +16,7 @@ if __name__ == "__main__":
     im = torch.randn([2,3,64,64])
     EBML = EBMLearner(config)
 
+    answers = ["True","False"]
     outputs = EBML.ground_concept(im,["exist(scene())","exist(scene())"])
     print(outputs)
+    print(ground_results(outputs,answers))
