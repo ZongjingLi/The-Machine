90fdb9cd8d24c7a4600dd736c996aa1db4d55aec
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-29 08:45:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 import torch
 import torch.nn as nn
 
+from torch.utils.data import Dataset, DataLoader
+
 def ground_results(results,answers):
     outputs = []
     for i in range(len(results)):
-        assert answers[i] in results[i]["outputs"]
-        index_answer = results[i]["outputs"].index(answers[i])
-        outputs.append(results[i]["scores"][index_answer])
+        # discrete output probability distribution
+        if isinstance(results[i],dict):
+            assert answers[i] in results[i]["outputs"]
+            index_answer = results[i]["outputs"].index(answers[i])
+            outputs.append(results[i]["scores"][index_answer])
+        # continuous output, ground with additional prior
+        else:
+            target_answer = int(answers[i])
+            outputs.append( torch.log(torch.sigmoid((0.5-torch.abs(results[i] - target_answer) )/0.125)))
     return outputs
+
+def train_ebml(model,dataset,joint = False):
+    optimizer = torch.optim.Adam(model.parameters(),lr = 2e-4)
+
+    trainloader = DataLoader(dataset)
+    
+    for epoch in range(1000):
+        total_loss = 0
+        for sample in trainloader:
+            images    = sample["image"]
+            questions = sample["question"]
+            programs  = sample["program"]
+            answers   = sample["answer"]
+            
+            results = model.ground_concept(images,programs)
+
+            logprobs  = ground_results(results,answers)
+            
+            qa_loss = 0
+            for term in logprobs:qa_loss - term
+            
+            qa_loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
 if __name__ == "__main__":
     from config import *
@@ -16,8 +48,8 @@ if __name__ == "__main__":
 
     im = torch.randn([3,3,64,64])
     EBML = EBMLearner(config)
-
-    answers = ["True","True"]
-    outputs = EBML.ground_concept(im,["exist(scene())","exist(filter(scene(),red))","count(scene())"])
+    
+    answers = ["True","True","3"]
+    outputs = EBML.ground_concept(im,["exist(scene())","exist(filter(scene(),blue))","count(scene())"])
     print(outputs)
-    #print(ground_results(outputs,answers))
+    print(ground_results(outputs,answers))
