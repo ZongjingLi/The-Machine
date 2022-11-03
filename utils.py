fe80036661aef8df7c970e1476b4ce558edaf3fe
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-11-04 06:16:49 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,3 +2,17 @@ import torch
 import torch.nn as nn
 
 from moic.data_structure import *
+
+import networkx as nx
+
+def toNxGraph(func):
+    nodes = []
+    def break_node(node):
+        nodes.append(node.token)
+        for c in node.children:break_node(c)
+    break_node(func)
+    print(nodes)
+
+func = toFuncNode("exist(filter(scene(),red))")
+
+func_g = toNxGraph(func)
