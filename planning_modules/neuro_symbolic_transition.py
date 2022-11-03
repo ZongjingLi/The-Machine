c8b57831a7c8765c59a328e17e668b3d535dadb8
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-11-04 06:57:57 +0800

    Update neuro_symbolic_transition.py

--- planning_modules/neuro_symbolic_transition.py
+++ planning_modules/neuro_symbolic_transition.py
@@ -4,9 +4,14 @@ import torch.nn as nn
 class NSAbstractModel(nn.Module):
     def __init__(self):
         super().__init__()
+        self.unary_predicates  = None
+        self.binary_predicates = None
 
     def forward(self,x):
         return x
 
     def abstract(state,soft = False):
+        # an abstract state is a diction with:
+        # - a set of entities with center and offsets 1e-6
+        # - a nullary entity that encodes some global info.
         return 0
