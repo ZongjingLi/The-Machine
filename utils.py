9f34bbc79cef106958613bae3e2a66d7fd9b7979
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-11-04 06:20:10 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,14 +5,28 @@ from moic.data_structure import *
 
 import networkx as nx
 
+import matplotlib.pyplot as plt
+
 def toNxGraph(func):
     nodes = []
+    edges = []
     def break_node(node):
-        nodes.append(node.token)
-        for c in node.children:break_node(c)
+        nodes.append(node.token);
+        for c in node.children:
+            edges.append([node.token,c.token])
+            break_node(c)
     break_node(func)
-    print(nodes)
+    func_graph = nx.DiGraph()
+    func_graph.add_nodes_from(nodes)
+    func_graph.add_edges_from(edges)
+    return func_graph
+    
 
 func = toFuncNode("exist(filter(scene(),red))")
 
 func_g = toNxGraph(func)
+
+print(func_g)
+nx.draw_networkx(func_g)
+
+plt.show()
